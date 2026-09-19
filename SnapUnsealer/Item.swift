fa0e2046c92de381dcdfd45d70aa98f6422be781
Nam Nguyen <nam.nguyen@cycastic.net>
2026-09-19 15:59:39 +0700

    Initial Commit

---
+++ SnapUnsealer/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SnapUnsealer
+//
+//  Created by Nam Nguyen on 19/9/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
